358bc1f45a17b9b4f43f42f589f9564836d8569c
Lior Degu <lior.degu@gmail.com>
2024-04-13 19:29:37 +0300

    pub-sub

--- main.go
+++ main.go
@@ -2,54 +2,123 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/websocket"
 )
 
 var client *redis.Client
+var pubsub *redis.PubSub
+var Users map[string]*websocket.Conn
+var upgrader = websocket.Upgrader{}
 
-const playerPrefix = "player-"
-const players = "players"
-const leaderboard = "leaderboard"
+const chatChannel = "chats"
 
 func init() {
-	client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
-	ctx := context.Background()
-
-	if err := client.Ping(ctx).Err(); err != nil {
-		log.Fatalf("failed to connect to redis.\nerror message - %s.", err)
-	}
-
-	if err := client.Del(ctx, players).Err(); err != nil {
-		log.Fatalf("failed to connect to delete players set.\nerror message - %s.", err)
-	}
-
-	if err := client.Del(ctx, leaderboard).Err(); err != nil {
-		log.Fatalf("failed to connect to delete leaderboard sorted set.\nerror message - %s.", err)
-	}
-
-	for i := 1; i <= 10; i++ {
-		err := client.SAdd(ctx, players, playerPrefix+strconv.Itoa(i)).Err()
-		if err != nil {
-			log.Fatalf("failed to add player to players set.\nerror message - %s.", err)
-		}
-	}
-
-	log.Println("successfuly connected to redis.")
+	Users = map[string]*websocket.Conn{}
 }
 
 func main() {
-	r := http.NewServeMux()
+	ctx := context.Background()
+	client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
 
-	r.HandleFunc("GET /health", health)
-	r.HandleFunc("GET /leaderboard", getLeaderboard)
-	r.HandleFunc("GET /top", getTopN)
-	r.HandleFunc("POST /add-player", addPlayer)
-	r.HandleFunc("POST /play", play)
+	if err := client.Ping(ctx).Err(); err != nil {
+		log.Fatal("ping failed. could not connect", err)
+	}
+	startChatBroadcaster()
 
-	log.Println("HTTP server started on port 8080...")
-	log.Fatalln(http.ListenAndServe(":8080", r))
+	http.HandleFunc("/chat/", chat)
+	server := http.Server{Addr: ":8080", Handler: nil}
+
+	go func() {
+		fmt.Println("started server")
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("failed to start server", err)
+		}
+	}()
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+	<-exit
+
+	fmt.Println("exit signalled")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	//clean up all connected connections
+	for _, conn := range Users {
+		conn.Close()
+	}
+
+	pubsub.Unsubscribe(context.Background(), chatChannel)
+	pubsub.Close()
+
+	server.Shutdown(ctx)
+
+	fmt.Println("application shut down")
+}
+
+func chat(w http.ResponseWriter, r *http.Request) {
+	user := strings.TrimPrefix(r.URL.Path, "/chat/")
+
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	Users[user] = c
+	fmt.Println(user, "in chat")
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			//fmt.Println("read message error", err)
+			_, ok := err.(*websocket.CloseError)
+			if ok {
+				fmt.Println("connection closed by", user)
+				err := c.Close()
+				if err != nil {
+					fmt.Println("error closing ws connection", err)
+				}
+				delete(Users, user)
+				fmt.Println("closed websocket connection and removed user session")
+			}
+			break
+		}
+		err = client.Publish(context.Background(), chatChannel, user+":"+string(message)).Err()
+		if err != nil {
+			fmt.Println("publish failed", err)
+		}
+	}
+}
+
+func startChatBroadcaster() {
+	go func() {
+		fmt.Println("listening to messages")
+		pubsub = client.Subscribe(context.Background(), chatChannel)
+		messages := pubsub.Channel()
+		for message := range messages {
+			from := strings.Split(message.Payload, ":")[0]
+			//broadcast to all
+			for user, peer := range Users {
+				if from != user {
+					peer.WriteMessage(websocket.TextMessage, []byte(message.Payload))
+				}
+			}
+		}
+	}()
 }
